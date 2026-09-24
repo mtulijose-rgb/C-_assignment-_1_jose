e7cd262c6e1d24d875bc69ac1269393dd09baf0c
mtulijose-rgb <mtulijose@gmail.com>
2026-09-24 16:22:47 +0300

    Add files via upload

---
+++ user_input.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    //Declare variables
+    float height = 1.89;
+    float balance = 300.00;
+    char phone[] = "0797559661";
+    printf("My details\n");
+    printf("Height: %.2f m\n",height);
+    printf("Bank balance: KSH %.2f\n,balance");
+    printf("Phone number: %s\n",phone);
+    return 0;
+}
