f49c826e31374f4712c9c93f7d971456c87bb394
mtulijose-rgb <mtulijose@gmail.com>
2026-09-24 15:51:04 +0300

    Add files via upload

---
+++ newfile.cxx
@@ -0,0 +1,14 @@
+#include <stdio.h>
+ int main(){
+    //Display academic units offered this semester; printf("units offered this semester\n\n"); 
+
+printf("1.introduction to computer applications\n");
+printf("2.fundamentals of digital technology and artificial intelligence\n");
+printf("3.analog electronics\n");
+printf("4.essentials of accounting\n");
+printf("5.mathematics for science\n");
+printf("6.introduction to programming and algorithms\n");
+printf("7.communication and information literacy skills\n");
+printf("8.computer systems and organizations\n");
+ return 0; 
+ }
